6e77f0eb4b1e80f1248ff1702ce2c45bf74ade6e
Tenzin Choeying <117798498+choeying-10@users.noreply.github.com>
2025-11-19 09:30:44 +0530

    Update index.js
    absolute path fixed

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,7 +23,10 @@ const db = new pg.Client({
 db.connect();
 
 app.use(bodyParser.urlencoded({extended : true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 async function checkVisited()
 {
